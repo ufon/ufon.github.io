d0b9759aee9f519e97d4b7b254d6db6e47efd074
Anton Postoyalko <ufon.exe@yandex.ru>
2018-12-21 05:18:48 +0300

    add coolpoll

---
+++ coolpoll/precache-manifest.4289cc04e188e1d740a161a4a732acea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "07dae4d803bfe08a6a92",
+    "url": "/coolpoll/static/js/runtime~main.07dae4d8.js"
+  },
+  {
+    "revision": "19319fcd1c30979e9785",
+    "url": "/coolpoll/static/js/main.19319fcd.chunk.js"
+  },
+  {
+    "revision": "3e5b36cce16c025cb5d2",
+    "url": "/coolpoll/static/js/1.3e5b36cc.chunk.js"
+  },
+  {
+    "revision": "3e5b36cce16c025cb5d2",
+    "url": "/coolpoll/static/css/1.2ded0261.chunk.css"
+  },
+  {
+    "revision": "109a5261ceb97948aadf4f66a500df0b",
+    "url": "/coolpoll/index.html"
+  }
+];
